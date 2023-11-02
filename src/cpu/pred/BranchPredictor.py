c5b38fb4219a67f4be8b47fd9d0de519206775d4
220031-Miao Heng <1098011272@qq.com>
2023-11-02 17:06:34 +0800

    Modefy  2bit_local.py BTB_entry

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -79,7 +79,8 @@ class LocalBP(BranchPredictor):
     cxx_class = "gem5::branch_prediction::LocalBP"
     cxx_header = "cpu/pred/2bit_local.hh"
 
-    localPredictorSize = Param.Unsigned(2048, "Size of local predictor")
+    # localPredictorSize = Param.Unsigned(2048, "Size of local predictor")
+    localPredictorSize = Param.Unsigned(512, "Size of local predictor")
     localCtrBits = Param.Unsigned(2, "Bits per counter")
 
 
