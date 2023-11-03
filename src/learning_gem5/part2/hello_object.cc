6835599e1e71e498c11a8cdf78f09a67515037a0
220031-Miao Heng <1098011272@qq.com>
2023-11-03 21:46:53 +0800

    Debug

--- src/learning_gem5/part2/hello_object.cc
+++ src/learning_gem5/part2/hello_object.cc
@@ -1,11 +1,41 @@
 #include "learning_gem5/part2/hello_object.hh"
+#include "base/trace.hh"
+#include "debug/HelloExample.hh"
 
 #include <iostream>
 
 namespace gem5
 {
     HelloObject::HelloObject(const HelloObjectParams &params) :
-        SimObject(params) {
-            std::cout << "Hello World! From a SimObject!" << std::endl;
+        SimObject(params), event([this]{processEvent();}, name()),
+        latency(100), timesLeft(10)
+        {     // event可以执行任何函数，通过this捕捉并调用其中内容
+ //           std::cout << "Hello World! From a SimObject!" << std::endl;
+            DPRINTF(HelloExample, "Created the hello object\n");
         }
+
+    // void 
+    //     HelloObject::processEvent()
+    //     {
+    //         DPRINTF(HelloExample, "Hello world! Processing the event!\n");
+    //     }
+
+    void
+        HelloObject::startup()
+        {
+            schedule(event, latency);    //：cppschedule函数,只允许调度在未来某个时间。
+        }
+
+    void
+    HelloObject::processEvent()
+    {
+        timesLeft--;
+        DPRINTF(HelloExample, "Hello world! Processing the event! %d left\n", timesLeft);
+
+        if (timesLeft <= 0) {
+            DPRINTF(HelloExample, "Done firing!\n");
+        } else {
+            schedule(event, curTick() + latency);
+        }
+    }
 } // namespace gem5
--- src/learning_gem5/part2/hello_object.hh
+++ src/learning_gem5/part2/hello_object.hh
@@ -9,9 +9,21 @@ namespace gem5
 
     class HelloObject : public SimObject
     {
+        private:
+            void processEvent();          // 定义的函数
+
+            EventFunctionWrapper event;   // 执行任何函数，需要两个参数
+
+            const Tick latency;
+            
+            int timesLeft;
+
         public:
             HelloObject(const HelloObjectParams &p);
+
+            void startup() override;
     };
+
 } // namespace gem5
 
 #endif // __LEARNING_GEM5_HELLO_OBJECT_HH__
